61605f32d1791804d77d3df01e4b19b8519f644c
LEE026 <lhs456852@gmail.com>
2023-03-20 22:10:17 +0900

    Post인터페이스추가

---
+++ src/main/java/com/backend/backend/repository/postRepository/PostRepository.java
@@ -0,0 +1,21 @@
+package com.backend.backend.repository.postRepository;
+
+import com.backend.backend.domain.Member;
+import com.backend.backend.domain.Post;
+
+public interface PostRepository {
+    /**
+     * Post객체 전달시 레파지토리에 저장됨
+     * @param post
+     * @return
+     */
+    Long save(Post post);
+
+    /**
+     * id를 이용해서 Post객체 검색
+     * @param id
+     * @return
+     */
+    Post findPostById(Long id);
+
+}
