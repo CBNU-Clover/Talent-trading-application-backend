1113723db33a3f5e5b2d15d83b7b1143260dcec9
LEE026 <lhs456852@gmail.com>
2023-03-22 20:00:08 +0900

    MysqlReviewRepository 제작

---
+++ src/main/java/com/backend/backend/repository/reviewRepository/MysqlReviewRepository.java
@@ -0,0 +1,30 @@
+package com.backend.backend.repository.reviewRepository;
+
+import com.backend.backend.domain.Review;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+
+@Repository
+@RequiredArgsConstructor
+public class MysqlReviewRepository implements ReviewRepository{
+
+    private final EntityManager em;
+    @Override
+    public Long save(Review review) {
+        em.persist(review);
+        return review.getId();
+    }
+
+    @Override
+    public Review findReviewById(Long id) {
+        return em.find(Review.class, id);
+    }
+
+    @Override
+    public void deleteReviewById(Long id) {
+        Review review = em.find(Review.class, id);
+        em.remove(review);
+    }
+}
