12b8c8cf4abb73fcca102de7340fa88b4693364d
LEE026 <lhs456852@gmail.com>
2023-03-29 19:16:32 +0900

    잘못된 어노테이션 삭제

--- src/main/java/com/backend/backend/repository/memberRepository/MysqlMemberRepository.java
+++ src/main/java/com/backend/backend/repository/memberRepository/MysqlMemberRepository.java
@@ -14,7 +14,6 @@ import java.util.List;
 import static com.backend.backend.domain.QPost.post;
 
 @Repository
-@RequiredArgsConstructor
 public class MysqlMemberRepository implements MemberRepository{
     
     private final EntityManager em;
