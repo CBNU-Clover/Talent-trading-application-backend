054340e04bbe40da6b0878fa8da64166d5fe1856
LEE026 <lhs456852@gmail.com>
2023-04-04 20:56:44 +0900

    deletePost테스트

---
+++ src/test/java/com/backend/backend/service/PostServiceTest.java
@@ -0,0 +1,86 @@
+package com.backend.backend.service;
+
+import com.backend.backend.domain.Member;
+import com.backend.backend.domain.Post;
+import com.backend.backend.exception.PermissionDeniedException;
+import com.backend.backend.repository.memberRepository.MemberRepository;
+import com.backend.backend.repository.postRepository.PostRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@Transactional
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class PostServiceTest {
+
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Autowired
+    private PostService postService;
+
+    Long initMemberId;
+    Long initPostId1;
+    Long initPostId2;
+
+    @BeforeEach
+    void init(){
+        MockitoAnnotations.initMocks(this);
+        Member member = Member.builder()
+                .name("1")
+                .nickName("11")
+                .passWord("ghj")
+                .build();
+        initMemberId = memberRepository.save(member);
+
+        Post post1 = Post.builder()
+                .writer(memberRepository.findMemberById(initMemberId))
+                .postName(".")
+                .content(",")
+                .build();
+
+        initPostId1=postRepository.save(post1);
+
+
+        Post post2 = Post.builder()
+                .writer(memberRepository.findMemberById(initMemberId))
+                .postName(".")
+                .content(",")
+                .build();
+        initPostId2=postRepository.save(post2);
+    }
+
+    @Test
+    void deletePost() {
+        Member member = Member.builder()
+                .name("1")
+                .nickName("111")
+                .passWord("ghj")
+                .build();
+        memberRepository.save(member);
+
+        org.junit.jupiter.api.Assertions.assertThrows(PermissionDeniedException.class, ()->{
+            postService.deletePost(initPostId1,member);
+        });
+    }
+}
