777d47077b863f694409ea610888b01c3da5d76f
LEE026 <lhs456852@gmail.com>
2023-04-04 19:37:45 +0900

    ReviewService제작

---
+++ src/main/java/com/backend/backend/service/ReviewService.java
@@ -0,0 +1,46 @@
+package com.backend.backend.service;
+
+
+import com.backend.backend.domain.Review;
+import com.backend.backend.repository.postRepository.PostRepository;
+import com.backend.backend.repository.reviewRepository.ReviewRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ReviewService {
+
+    private final ReviewRepository reviewRepository;
+
+
+    /**
+     * review객체 전달시 저장
+     * @param review
+     * @return
+     */
+    @Transactional
+    public Long writeReview(Review review){
+        return reviewRepository.save(review);
+    }
+
+    /**
+     * review를 구분 가능한 방법이 id뿐이기에 id로 찾음
+     * @param id
+     * @return
+     */
+    public Review readReview(Long id){
+        return reviewRepository.findReviewById(id);
+    }
+
+    /**
+     * review를 구분 가능한 방법이 id뿐이기에 id로 검색하여 종료
+     * @param id
+     */
+    @Transactional
+    public void deleteReview(Long id){
+        reviewRepository.deleteReviewById(id);
+    }
+}
