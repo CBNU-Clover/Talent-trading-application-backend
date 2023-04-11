8f440a0c43d41fbf5526d87d7a6317a80d9e5748
(shinhyo) <(tlsgyals1216@gmail.com)>
2023-04-12 02:28:30 +0900

    닉네임 , 이메일 중복체크 API 제작 완료(ResponseEntity<String>타입으로 변경)

---
+++ src/main/java/com/backend/backend/dto/memberdto/BooleanDuplicateCheckDTO.java
@@ -0,0 +1,15 @@
+package com.backend.backend.dto.memberdto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class BooleanDuplicateCheckDTO {
+    private boolean flag;
+}
