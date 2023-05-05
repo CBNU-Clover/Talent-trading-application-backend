ac6804de8e787ece0695a453373a02075fa666f4
LEE026 <lhs456852@gmail.com>
2023-05-05 15:57:35 +0900

    test: chargePoint 테스트 제작

---
+++ src/test/java/com/backend/backend/service/PointServiceTest.java
@@ -0,0 +1,58 @@
+package com.backend.backend.service;
+
+import com.backend.backend.domain.member.Member;
+import com.backend.backend.repository.memberRepository.MemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@Transactional
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class PointServiceTest {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private PointService pointService;
+    String memberNickname1="testMember1";
+    String memberNickname2="testMember2";
+    @BeforeEach
+    void setUp() {
+        Member member1 = Member.builder()
+                .name("1")
+                .nickname(memberNickname1)
+                .passWord("ghj")
+                .build();
+        memberRepository.save(member1);
+
+        Member member2 = Member.builder()
+                .name("1")
+                .nickname(memberNickname2)
+                .passWord("ghj")
+                .build();
+        memberRepository.save(member2);
+    }
+
+    @Test
+    void chargePoint() {
+        Member member = memberRepository.findMemberByNickname(memberNickname1);
+
+        Long amount=50L;
+        pointService.chargePoint(member.getNickname(),amount);
+
+        Assertions.assertThat(member.getPoint()).isEqualTo(amount);
+    }
+
+
+}
