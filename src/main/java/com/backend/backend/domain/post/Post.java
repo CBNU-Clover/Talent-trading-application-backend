99b4fafb9fa36a88cf4dcc3bae25ee9d0215e7da
LEE026 <lhs456852@gmail.com>
2023-05-10 18:55:09 +0900

    feature: PointDetail 추가, Post의 @JSONIgnore 색제

--- src/main/java/com/backend/backend/domain/post/Post.java
+++ src/main/java/com/backend/backend/domain/post/Post.java
@@ -24,7 +24,6 @@ public class Post {
     @GeneratedValue
     private Long id;
 
-    @JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id",nullable = false)
     Member writer;
