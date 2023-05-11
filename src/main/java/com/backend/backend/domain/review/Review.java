02f83513d73ab3da94ae6a1e5688ce7c02941638
LEE026 <lhs456852@gmail.com>
2023-05-11 20:27:37 +0900

    feature: Review의 builder에 nullable 컬럼들에 대한 null이 아님을 확인하는 과정 추가

--- src/main/java/com/backend/backend/domain/review/Review.java
+++ src/main/java/com/backend/backend/domain/review/Review.java
@@ -2,6 +2,7 @@ package com.backend.backend.domain.review;
 
 import com.backend.backend.domain.member.Member;
 import com.backend.backend.domain.post.Post;
+import com.backend.backend.exception.NotExistException;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -10,6 +11,7 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.util.Assert;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -41,6 +43,12 @@ public class Review {
 
     @Builder
     public Review(Post post, Member writer, String content) {
+        if(post==null){
+            throw new NotExistException("해당되는 게시글이 없습니다");
+        }
+        if(writer==null){
+            throw new NotExistException("작성자가 없습니다");
+        }
         this.post = post;
         this.writer = writer;
         this.content = content;
