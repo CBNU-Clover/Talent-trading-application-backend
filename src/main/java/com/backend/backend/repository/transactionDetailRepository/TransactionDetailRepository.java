3706fe755216cffc621a0d5df9981fafe361c616
LEE026 <lhs456852@gmail.com>
2023-05-11 20:45:49 +0900

    comment: TransactionDetailRepository 주석 추가

--- src/main/java/com/backend/backend/repository/transactionDetailRepository/TransactionDetailRepository.java
+++ src/main/java/com/backend/backend/repository/transactionDetailRepository/TransactionDetailRepository.java
@@ -8,7 +8,17 @@ import java.util.List;
 
 public interface TransactionDetailRepository {
 
+    /**
+     * 재능거래 기록 저장
+     * @param transactionDetail 재능거래 기록
+     * @return
+     */
     Long save(TransactionDetail transactionDetail);
 
+    /**
+     * 해당회원의 재능거래 기록을 반환
+     * @param member 회원 객체
+     * @return
+     */
     List<TransactionDetail> findDetailsByMember(Member member);
 }
