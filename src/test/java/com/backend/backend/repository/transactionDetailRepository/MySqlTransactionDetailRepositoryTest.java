512a59b9dd8df5e985b43f6ab7b671961883db72
LEE026 <lhs456852@gmail.com>
2023-05-12 10:43:06 +0900

    Test: TransactionDetailRepository 테스트 수정 - member의 유니크 항목의 중복 제거

--- src/test/java/com/backend/backend/repository/transactionDetailRepository/MySqlTransactionDetailRepositoryTest.java
+++ src/test/java/com/backend/backend/repository/transactionDetailRepository/MySqlTransactionDetailRepositoryTest.java
@@ -10,10 +10,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -40,24 +42,32 @@ class MySqlTransactionDetailRepositoryTest {
                 .build();
         memberRepository.save(member1);
         member2 = Member.builder()
-                .name("415646556456")
-                .nickname("huysdagsibib")
-                .passWord("5456")
-                .email("566511561sd1")
+                .name("41564655ggg6456")
+                .nickname("huysdaggggsibib")
+                .passWord("5456dff")
+                .email("566511561gggsd1")
                 .build();
         memberRepository.save(member2);
     }
 
     @Test
     void save() {
-        TransactionDetail detail = TransactionDetail.builder()
+        TransactionDetail detail1 = TransactionDetail.builder()
                 .buyer(member1)
                 .seller(member2)
                 .build();
-        Long id = transactionDetailRepository.save(detail);
+        Long id1 = transactionDetailRepository.save(detail1);
+        TransactionDetail detail2 = TransactionDetail.builder()
+                .buyer(member2)
+                .seller(member1)
+                .build();
+        Long id2 = transactionDetailRepository.save(detail2);
 
-        Assertions.assertThat(transactionDetailRepository.findDetailById(id))
-                .isSameAs(detail);
+        Assertions.assertThat(transactionDetailRepository.findDetailById(id1))
+                .isSameAs(detail1);
+
+        Assertions.assertThat(transactionDetailRepository.findDetailById(id2))
+                .isSameAs(detail2);
     }
 
     @Test
@@ -68,17 +78,16 @@ class MySqlTransactionDetailRepositoryTest {
                 .build();
         transactionDetailRepository.save(detail1);
         TransactionDetail detail2 = TransactionDetail.builder()
-                .buyer(member2)
-                .seller(member1)
+                .buyer(member1)
+                .seller(member2)
                 .build();
         transactionDetailRepository.save(detail2);
 
-        Assertions.assertThat(
-                        transactionDetailRepository.findDetailsByMember(member1).get(0))
-                .isSameAs(detail1);
-        Assertions.assertThat(
-                        transactionDetailRepository.findDetailsByMember(member1).get(1))
-                .isSameAs(detail2);
+        List<TransactionDetail> details =
+                transactionDetailRepository.findDetailsByMember(member1);
 
+        Assertions.assertThat(details.size()).isEqualTo(2);
+        Assertions.assertThat(details.get(0)).isSameAs(detail1);
+        Assertions.assertThat(details.get(1)).isSameAs(detail2);
     }
 }
