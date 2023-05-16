c04916d4fe02855ff4d6b7c17edf17d435f072cf
LEE026 <lhs456852@gmail.com>
2023-05-16 20:01:00 +0900

    test: RakingService 제작

---
+++ src/main/java/com/backend/backend/service/RakingService.java
@@ -0,0 +1,21 @@
+package com.backend.backend.service;
+
+import com.backend.backend.domain.member.Rating;
+import com.backend.backend.domain.member.RatingCategory;
+import com.backend.backend.repository.ratingRepository.RatingRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class RakingService {
+    private final RatingRepository ratingRepository;
+
+    List<Rating> getTopRating(RatingCategory category){
+        return ratingRepository.getTopRanking(category);
+    }
+}
