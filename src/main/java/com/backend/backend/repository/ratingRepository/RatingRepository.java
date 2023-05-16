28409f3f11383622db56db38454edba8e9ca270d
LEE026 <lhs456852@gmail.com>
2023-05-16 19:14:21 +0900

    feature: 레이팅에 점수를 더해주는 메소드를 제작

--- src/main/java/com/backend/backend/repository/ratingRepository/RatingRepository.java
+++ src/main/java/com/backend/backend/repository/ratingRepository/RatingRepository.java
@@ -8,8 +8,16 @@ import java.util.List;
 public interface RatingRepository {
 
     /**
-     * 헤당 카테고리의 탑 20의 랭킹을 반환해 주는 메소드
+     * 해당 카테고리의 탑 20의 랭킹을 반환해 주는 메소드
+     * @param category
      * @return
      */
     List<Rating> getTopRanking(RatingCategory category);
+
+    /**
+     * 해당 카테고리에 amount 점수를 추가, 만약 해당카테고리에 대한 점수가 처음 들어가는 것이라면 해당 튜플 생성
+     * @param category
+     * @param amount 추가될 스코어 점수
+     */
+    void addRating(RatingCategory category,Long amount);
 }
