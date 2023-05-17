da459873ad2db741425421fbcdff7d42eac95d8c
LEE026 <lhs456852@gmail.com>
2023-05-17 18:35:44 +0900

    feature: 랭킹 api 제작

---
+++ src/main/java/com/backend/backend/controller/ranking/dto/ResponseRaking.java
@@ -0,0 +1,11 @@
+package com.backend.backend.controller.ranking.dto;
+
+import com.backend.backend.domain.member.Rating;
+import lombok.Data;
+
+@Data
+public class ResponseRaking {
+
+    public ResponseRaking(Rating rating) {
+    }
+}
---
+++ src/main/java/com/backend/backend/controller/ranking/dto/ResponseRakingList.java
@@ -0,0 +1,20 @@
+package com.backend.backend.controller.ranking.dto;
+
+import com.backend.backend.domain.member.Rating;
+import com.backend.backend.domain.member.RatingCategory;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class ResponseRakingList {
+    List<ResponseRaking> rankings;
+
+    public ResponseRakingList(List<Rating> rankings) {
+        rankings = new ArrayList<>();
+        for(Rating rating: rankings){
+            this.rankings.add(new ResponseRaking(rating));
+        }
+    }
+}
