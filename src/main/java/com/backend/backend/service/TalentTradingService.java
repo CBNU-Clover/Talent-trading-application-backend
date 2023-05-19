dffb7706f514bf822a83c47a2ec553ac185d8746
LEE026 <lhs456852@gmail.com>
2023-05-19 11:05:44 +0900

    feature: TalentTradingService 제작

---
+++ src/main/java/com/backend/backend/service/TalentTradingService.java
@@ -0,0 +1,34 @@
+package com.backend.backend.service;
+
+import com.backend.backend.domain.member.Member;
+import com.backend.backend.domain.post.Post;
+import com.backend.backend.domain.transactionDetail.TransactionDetail;
+import com.backend.backend.repository.memberRepository.MemberRepository;
+import com.backend.backend.repository.postRepository.PostRepository;
+import com.backend.backend.repository.transactionDetailRepository.TransactionDetailRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TalentTradingService {
+    private final MemberRepository memberRepository;
+    private final PostRepository postRepository;
+    private final PointService pointService;
+    private final TransactionDetailRepository transactionDetailRepository;
+
+    public void talentTrading(Long postId, String buyerNickname){
+        Member buyer = memberRepository.findMemberByNickname(buyerNickname);
+        Post post = postRepository.findPostById(postId);
+
+        pointService.remittancePoint(buyerNickname,post.getWriter().getNickname(),post.getPrice());
+
+        TransactionDetail transactionDetail = TransactionDetail.builder()
+                .seller(post.getWriter())
+                .buyer(buyer)
+                .build();
+        transactionDetailRepository.save(transactionDetail);
+
+    }
+
+}
