633a793b06a283b873cc5248a78b270990eafeb7
LEE026 <lhs456852@gmail.com>
2023-06-19 11:47:46 +0900

    feature: createPost 픽스쳐 제작

--- src/test/java/com/backend/backend/Fixture.java
+++ src/test/java/com/backend/backend/Fixture.java
@@ -1,6 +1,7 @@
 package com.backend.backend;
 
 import com.backend.backend.domain.member.Member;
+import com.backend.backend.domain.post.Post;
 
 public class Fixture {
 
@@ -10,6 +11,15 @@ public class Fixture {
                 .nickname("TestNickName"+postFix)
                 .passWord("5456")
                 .email("566511561sd1"+postFix+"@test.com")
+                .build();
+    }
+
+    public static Post createPost(Member seller,Long price){
+        return Post.builder()
+                .writer(seller)
+                .postName("testTest")
+                .content(",")
+                .price(price)
                 .build();
     }
 }
