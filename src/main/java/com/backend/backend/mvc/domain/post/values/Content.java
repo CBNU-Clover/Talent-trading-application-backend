9ea2ea5d5dfc20bb12c698471000170d51517b82
LEE026 <lhs456852@gmail.com>
2023-06-20 16:29:34 +0900

    feature: 값객체 Content 제작

--- src/main/java/com/backend/backend/mvc/domain/post/values/Content.java
+++ src/main/java/com/backend/backend/mvc/domain/post/values/Content.java
@@ -1,4 +1,48 @@
 package com.backend.backend.mvc.domain.post.values;
 
+import java.util.Objects;
+
 public class Content {
+
+    private String content;
+
+    /**
+     * @Nullary-Constructor JPA 기본 생성자로 외부 패키지에서 호출하면 안됨
+     */
+    protected Content() {
+
+    }
+
+    private Content(String content) {
+        validateCheck(content);
+        this.content = content;
+    }
+
+    private void validateCheck(String content) {
+        if (content == null) {
+            throw new IllegalArgumentException("게시글 제목을 입력해주세요.");
+        }
+    }
+
+    public static Content from(String content) {
+        return new Content(content);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Content content1 = (Content) o;
+        return Objects.equals(content, content1.content);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(content);
+    }
+
+    @Override
+    public String toString() {
+        return content;
+    }
 }
