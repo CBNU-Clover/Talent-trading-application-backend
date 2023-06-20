7ef7baa793a478a63673c14a51ad7260a9e0a06c
LEE026 <lhs456852@gmail.com>
2023-06-20 16:33:36 +0900

    feature: 값객체 Price 제작

--- src/main/java/com/backend/backend/mvc/domain/post/values/Price.java
+++ src/main/java/com/backend/backend/mvc/domain/post/values/Price.java
@@ -1,4 +1,36 @@
 package com.backend.backend.mvc.domain.post.values;
 
+import com.backend.backend.mvc.domain.pointDetail.values.Balance;
+
+import javax.persistence.Column;
+
 public class Price {
+    @Column(name = "price")
+    private Long amount;
+
+    /**
+     * @Nullary-Constructor JPA 기본 생성자로 외부 패키지에서 호출하면 안됨
+     */
+    protected Price() {
+
+    }
+
+    private Price(Long amount) {
+        validateCheck(amount);
+        this.amount = amount;
+    }
+
+    private void validateCheck(Long amount) {
+        if (amount == null) {
+            throw new IllegalArgumentException("값을 입력해주세요.");
+        }
+        if (amount < 0) {
+            throw new IllegalArgumentException("값이 0보다 커야 합니다");
+        }
+    }
+
+    public static Price from(Long amount) {
+        return new Price(amount);
+    }
+
 }
