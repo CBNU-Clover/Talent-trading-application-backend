519b38573376c58663b6599c28e86eb8e4fb5576
LEE026 <lhs456852@gmail.com>
2023-06-20 15:07:35 +0900

    feature: PointDetail에 들어갈 값인 Recipient 제작

--- src/main/java/com/backend/backend/mvc/domain/pointDetail/values/Recipient.java
+++ src/main/java/com/backend/backend/mvc/domain/pointDetail/values/Recipient.java
@@ -1,4 +1,50 @@
 package com.backend.backend.mvc.domain.pointDetail.values;
 
+import com.backend.backend.mvc.domain.member.values.Name;
+
+import java.util.Objects;
+
 public class Recipient {
+
+    private String name;
+
+    /**
+     * @Nullary-Constructor JPA 기본 생성자로 member 외부 패키지에서 호출하면 안됨
+     */
+    protected Recipient() {
+
+    }
+
+    private Recipient(String name) {
+        validateCheck(name);
+        this.name = name;
+    }
+
+    private void validateCheck(String name) {
+        if (name == null) {
+            throw new IllegalArgumentException("수신처를 입력해주세요.");
+        }
+    }
+
+    public static Recipient from(String name) {
+        return new Recipient(name);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Recipient recipient = (Recipient) o;
+        return Objects.equals(name, recipient.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
