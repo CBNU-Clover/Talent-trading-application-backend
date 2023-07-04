85cef104b3e69d10d5434285f7dc100af3e72717
LEE026 <lhs456852@gmail.com>
2023-07-04 16:28:04 +0900

    feature: 인기게시글을 저장할 엔티티 제작
    - post의 제목이나 가격이 바뀔경우 같이 바뀌어야 되기에 별도로 저장하지 않고 그 때마다 post에서 가져옴

---
+++ src/main/java/com/backend/backend/mvc/domain/popularPost/PopularPost.java
@@ -0,0 +1,33 @@
+package com.backend.backend.mvc.domain.popularPost;
+
+import com.backend.backend.mvc.domain.post.Post;
+import com.backend.backend.mvc.domain.post.values.PostName;
+import com.backend.backend.mvc.domain.post.values.Price;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PopularPost {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    private Post post;
+
+    /**
+     * 인기게시글 순서를 위한
+     */
+    private Long viewCount;
+
+    public PopularPost(Post post,Long viewCount) {
+        this.post = post;
+        this.viewCount = viewCount;
+    }
+}
