84662e7cc7f89f00c251eea68a62fb66db3a4167
LEE026 <lhs456852@gmail.com>
2023-07-04 19:47:53 +0900

    Test, fix: 스케줄러의 인기 게시글 선정 기능에 대한 테스트 제작 및 그에 따라 발견된 에러 수정

--- src/main/java/com/backend/backend/mvc/repository/popularPostRepository/DbPopularPostRepository.java
+++ src/main/java/com/backend/backend/mvc/repository/popularPostRepository/DbPopularPostRepository.java
@@ -34,6 +34,7 @@ public class DbPopularPostRepository implements PopularPostRepository{
     @Override
     public void deleteAllPopularPosts() {
         query.delete(popularPost).execute();
+        em.flush();
         em.clear();
     }
 
