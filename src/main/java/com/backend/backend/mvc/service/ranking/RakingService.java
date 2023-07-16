584beca6f183d06c1113f12ad389b449469829d3
LEE026 <lhs456852@gmail.com>
2023-07-16 12:34:13 +0900

    feature: 랭킹 서비스에 레이팅 policy 적용

--- src/main/java/com/backend/backend/mvc/service/ranking/RakingService.java
+++ src/main/java/com/backend/backend/mvc/service/ranking/RakingService.java
@@ -4,6 +4,7 @@ import com.backend.backend.mvc.domain.member.Member;
 import com.backend.backend.mvc.domain.rating.Rating;
 import com.backend.backend.mvc.domain.rating.values.RatingCategory;
 import com.backend.backend.mvc.repository.ratingRepository.RatingRepository;
+import com.backend.backend.mvc.service.ranking.ratingPolicy.RatingPolicy;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,13 +16,14 @@ import java.util.List;
 @Transactional(readOnly = true)
 public class RakingService {
     private final RatingRepository ratingRepository;
+    private final RatingPolicy ratingPolicy;
 
     public List<Rating> getTopRating(RatingCategory category){
         return ratingRepository.getTopRanking(category);
     }
 
     public void addRating(Member member,RatingCategory category){
-        Long amount = 0L;
+        Long amount = ratingPolicy.calculate();
         ratingRepository.addRating(member, category, amount);
     }
 }
