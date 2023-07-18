c9f72ad97d106cea229d076b3e5a50ea876ea50f
LEE026 <lhs456852@gmail.com>
2023-07-18 13:51:29 +0900

    feature: 별점에 사용할 값객체 제작

---
+++ src/main/java/com/backend/backend/mvc/domain/review/values/StarRating.java
@@ -0,0 +1,52 @@
+package com.backend.backend.mvc.domain.review.values;
+
+import com.backend.backend.mvc.domain.rating.values.Score;
+
+import java.util.Objects;
+
+public class StarRating {
+    
+    private Long rating=0L;
+
+    public StarRating() {
+    }
+
+    public StarRating(Long rating) {
+        validateCheck(rating);
+        this.rating = rating;
+    }
+
+    private void validateCheck(Long rating) {
+        if (rating == null) {
+            throw new IllegalArgumentException("값을 입력해주세요.");
+        }
+        if (rating < 0) {
+            throw new IllegalArgumentException("값이 0보다 커야 합니다");
+        }
+        if (rating > 5) {
+            throw new IllegalArgumentException("값이 5보다 작아야 합니다");
+        }
+    }
+
+    public static StarRating from(Long rating) {
+        return new StarRating(rating);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        StarRating that = (StarRating) o;
+        return Objects.equals(rating, that.rating);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(rating);
+    }
+
+    @Override
+    public String toString() {
+        return rating.toString();
+    }
+}
