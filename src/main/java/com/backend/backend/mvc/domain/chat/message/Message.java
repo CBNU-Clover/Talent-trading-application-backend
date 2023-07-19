08b36375aa5481f2a767baff9ad843439f2bb727
LEE026 <lhs456852@gmail.com>
2023-07-19 17:01:38 +0900

    test: 메세지의 속성 추가

--- src/main/java/com/backend/backend/mvc/domain/chat/message/Message.java
+++ src/main/java/com/backend/backend/mvc/domain/chat/message/Message.java
@@ -1,11 +1,17 @@
 package com.backend.backend.mvc.domain.chat.message;
 
+import com.backend.backend.mvc.domain.chat.chattingRoom.ChattingRoom;
+import com.backend.backend.mvc.domain.chat.message.values.MessageContent;
+import com.backend.backend.mvc.domain.member.Member;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -17,4 +23,26 @@ public class Message {
     @Column(name = "message_id")
     @GeneratedValue
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chatting_room",nullable = false)
+    private ChattingRoom room;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sender_id",nullable = false)
+    private Member sender;
+
+
+    @Embedded
+    private MessageContent content;
+
+    @CreatedDate
+    @LastModifiedDate
+    private LocalDateTime date;
+
+    public Message(ChattingRoom room, Member sender, String content) {
+        this.room = room;
+        this.sender = sender;
+        this.content = MessageContent.from(content);
+    }
 }
