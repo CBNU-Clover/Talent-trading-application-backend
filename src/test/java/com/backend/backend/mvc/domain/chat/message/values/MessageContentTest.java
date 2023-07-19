4e11a3502ca0f038b0c62b6f398aa6ffcc7d29d5
LEE026 <lhs456852@gmail.com>
2023-07-19 16:57:27 +0900

    test: 메세지의 content에 해당하는 값객체의 테스트 제작

---
+++ src/test/java/com/backend/backend/mvc/domain/chat/message/values/MessageContentTest.java
@@ -0,0 +1,56 @@
+package com.backend.backend.mvc.domain.chat.message.values;
+
+import com.backend.backend.mvc.domain.post.values.Content;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MessageContentTest {
+
+    @Test
+    @DisplayName("Null value throws IllegalArgumentException")
+    void from_null_throws_exception() {
+        String nullData = null;
+
+        assertThrows(IllegalArgumentException.class, () -> MessageContent.from(nullData));
+    }
+
+    @Test
+    @DisplayName("blink throws IllegalArgumentException")
+    void from_blink_throws_exception() {
+        String blinkData = "";
+
+        assertThrows(IllegalArgumentException.class, () -> MessageContent.from(blinkData));
+    }
+
+    @Test
+    @DisplayName("blink throws IllegalArgumentException")
+    void from_blink_throws_exception2() {
+        String blinkData = " ";
+
+        assertThrows(IllegalArgumentException.class, () -> MessageContent.from(blinkData));
+    }
+
+
+    @Test
+    @DisplayName("Valid return")
+    void from_valid_returns() {
+        String validValue = "test";
+
+        MessageContent content = MessageContent.from(validValue);
+
+        assertNotNull(content);
+        assertEquals(validValue, content.toString());
+    }
+
+    @Test
+    @DisplayName("toString returns string representation")
+    void to_string_returns_string_representation() {
+        String validValue = "test";
+
+        MessageContent validObject = MessageContent.from(validValue);
+
+        assertEquals(validValue, validObject.toString());
+    }
+}
