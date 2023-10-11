22e20cba3f97161c39e354211f301319cae02ee8
LEE026 <lhs456852@gmail.com>
2023-10-11 23:59:42 +0900

    hotfix: 인기게시글이 중복 되지 않도록 수정

--- src/main/java/com/backend/backend/common/scheduler/RedisViewCountScheduler.java
+++ src/main/java/com/backend/backend/common/scheduler/RedisViewCountScheduler.java
@@ -13,10 +13,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import javax.transaction.Transactional;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Set;
+import java.util.*;
 
 @Component
 @RequiredArgsConstructor
@@ -33,9 +30,11 @@ public class RedisViewCountScheduler {
     @Scheduled(initialDelay = 300000, fixedDelay = 300000)
     @Transactional
     public void updateVisitorData() {
+        System.out.println("스케줄러 시작");
         Set<String> keys = redisTemplate.keys(RedisKey.PostViewCount + "_*");
         //최소힙
         PriorityQueue<PopularPost> popularPosts = new PriorityQueue<>();
+        Set<Long> existPost = new HashSet<>();
 
         for(String key:keys){
             String[] parts = key.split("_");
@@ -49,21 +48,29 @@ public class RedisViewCountScheduler {
                 post.addViewCount(Long.parseLong(viewCount));
 
                 //인기게시글 선정
-                popularPosts.add(new PopularPost(post, Long.parseLong(viewCount)));
-                if(popularPosts.size()>5){
-                    popularPosts.poll();
+                if(!existPost.contains(post.getId())) {
+                    existPost.add(post.getId());
+                    popularPosts.add(new PopularPost(post, Long.parseLong(viewCount)));
+                    if (popularPosts.size() > 5) {
+                        popularPosts.poll();
+                    }
                 }
             }
             redisTemplate.delete(key);
         }
         popularPostRepository.getAllPopularPosts().forEach((popularPost)->{
-            popularPosts.add(new PopularPost(popularPost.getPost(), popularPost.getViewCount()-5));
-            if(popularPosts.size()>5){
-                popularPosts.poll();
+            if(!existPost.contains(popularPost.getPost().getId())) {
+                existPost.add(popularPost.getPost().getId());
+
+                popularPosts.add(new PopularPost(popularPost.getPost(), popularPost.getViewCount() - 5));
+                if (popularPosts.size() > 5) {
+                    popularPosts.poll();
+                }
             }
         });
 
         changePopularPost(popularPosts);
+        System.out.println("스케줄러 끝");
     }
 
 
