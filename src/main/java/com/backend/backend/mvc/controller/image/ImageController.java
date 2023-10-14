42637416280a7483fb9cece0ae7fc01aecfb23f6
LEE026 <lhs456852@gmail.com>
2023-10-14 13:38:03 +0900

    hotfix: 이미지가 없더라도 에러 발생이 안되도록 수정

--- src/main/java/com/backend/backend/mvc/controller/image/ImageController.java
+++ src/main/java/com/backend/backend/mvc/controller/image/ImageController.java
@@ -1,5 +1,6 @@
 package com.backend.backend.mvc.controller.image;
 
+import com.backend.backend.mvc.domain.image.Image;
 import com.backend.backend.mvc.repository.imageRepository.ImageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
@@ -16,7 +17,12 @@ public class ImageController {
     @GetMapping("/image/{id}")
     public ResponseEntity<byte[]> getImage(@PathVariable("id") Long imageId) {
         // byte 배열로부터 이미지 생성
-        byte[] imageBytes = imageRepository.findImageById(imageId).getImage();
+        Image image = imageRepository.findImageById(imageId);
+
+        byte[] imageBytes = null;
+        if(image!=null) {
+            imageBytes = image.getImage();
+        }
 
         // 이미지와 함께 응답 생성
         return ResponseEntity.ok()
