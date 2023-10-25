bdce560e308593d0501e6b1d7ef35e8d3b844a72
(shinhyo) <(tlsgyals1216@gmail.com)>
2023-10-25 12:31:11 +0900

    memberjoinrequest 생성자 추가

--- src/main/java/com/backend/backend/mvc/controller/member/memberdto/MemberJoinRequest.java
+++ src/main/java/com/backend/backend/mvc/controller/member/memberdto/MemberJoinRequest.java
@@ -4,7 +4,7 @@ package com.backend.backend.mvc.controller.member.memberdto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@AllArgsConstructor
+
 @Getter
 public class MemberJoinRequest {
 
@@ -14,6 +14,26 @@ public class MemberJoinRequest {
     private String passWord;
     private String phoneNumber;
     private byte[] image;
+
+    public MemberJoinRequest(String nickname, String email, String name, String passWord, String phoneNumber, byte[] image) {
+        this.nickname = nickname;
+        this.email = email;
+        this.name = name;
+        this.passWord = passWord;
+        this.phoneNumber = phoneNumber;
+        this.image=image;
+    }
+    public MemberJoinRequest(String nickname, String email, String name, String passWord, String phoneNumber) {
+        this.nickname = nickname;
+        this.email = email;
+        this.name = name;
+        this.passWord = passWord;
+        this.phoneNumber = phoneNumber;
+        this.image=null;
+    }
+
+
+
     public MemberJoinRequest() {
 
     }
